73bc9e5139d1c2fb0354b7452c61a29a0d8431cf
lrllzzr <43448423+lrllzzr@users.noreply.github.com>
2018-12-01 13:24:44 +0900

    d

--- odd.js
+++ odd.js
@@ -1,33 +1,61 @@
 
+
 $(document).ready(function(){
+  var left;
+  var right;
   var i=1;
-    $("#rule").click(function(){
+  var total=5000;
+    $("#roll").click(function(){
       var results =  Math.round(Math.random());
 
       if(results===0){
-
-        // $("#result").html("홀");
         $("ol").append('<div id="whole"><span id="round">'+i+('회차&nbsp')+'</span>'+"<li id='odd'>홀</li></div>");
-        // $("li").css('backgroundColor','blue');
         i=i+1;
+        return left=0;
 
       }
       else{
-        // $("#result").html("짝").css('backgroundColor','red');
         $("ol").append('<div id="whole"><span id="round">'+i+('회차&nbsp')+'</span>'+"<li id='even'>짝</li></div>");
-        // $("ol li").css('backgroundColor','red');
         i=i+1;
-
+        return left=1;
       }
+
     });
+        $("#oddclick").click(function(){
+          return right=0;
+        });
+        $("#oddclick").click(function(){
+          return right=1;
+        });
+
 });
 
+
+// $(document).ready(function(){
+// var total=5000;
+//     $("#oddclick").click(function(){
+//       var results =  Math.round(Math.random());
+//
+//       if(results===0){
+//         total=total+5000*0.95;
+//         $('#ax').html(total);
+//         right:0;
+//
+//       }
+//       else{
+//         total=total-5000;
+//         $('#ax').html(total);
+//         right=1;
+//       }
+//     });
+// });
 
 
 $(document).ready(function(){
   $( ".oddbtn1" ).click(function() {
   $( this ).toggleClass( "selected" );
   $(".evenbtn1").removeClass("selected");
+
   });
 
   $( ".evenbtn1" ).click(function() {
@@ -43,6 +71,9 @@ $(document).ready(function(){
   $( ".oddbtn2" ).click(function() {
   $( this ).toggleClass( "selected" );
   $(".evenbtn2").removeClass("selected");
+  });
+  $("#bet").click(function(){
+    alert('정상적으로 배팅 되었습니다');
   });
 
 });
